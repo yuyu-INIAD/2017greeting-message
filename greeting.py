115b69d9215eaee2dfc6b954b2af129c0251ad4b
yu1453 <s1f102102380@iniad.org>
2021-06-21 15:04:06 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,2 +1,14 @@
-def greet(name):
-    message = 'Hello, ' + name + '-san!'
+from datetime import datetime
+
+def greet():
+    hour = datetime.now().hour
+    if hour <= 11:
+        message = 'Good morning'
+    elif hour <= 17:
+        message = 'Hello'
+    else:
+        message = 'Good evening'
+    print(message)
+
+
+greet()
